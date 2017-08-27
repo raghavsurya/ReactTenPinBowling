25b74319f432c7c888a789e38183325989dc7be4
Raghav Surya <raghav2787@gmail.com>
2017-08-27 23:01:27 +0100

    ten pin bowling c#

---
+++ TenPinBowling/TenPinBowling/BowlingGame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenPinBowling
+{
+    public class BowlingGame
+    {
+     
+        private int[] rolls = new int[21];
+        private int currentRoll;
+
+        public void Roll(int pinsHit)
+        {
+            rolls[currentRoll++] = pinsHit;
+        }
+
+        public int GetTotalScore()
+        {   int totalScore = 0;
+            int roll = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(roll))
+                {
+                    totalScore += 10 + StrikeBonus(roll);
+                    roll++;
+                }
+                else if (IsSpare(roll))
+                {
+                    totalScore += 10 + SpareBonus(roll);
+                    roll += 2;
+                }
+                else
+                {
+                    totalScore += SummOfBallsInAFrame(roll);
+                    roll += 2;
+                }
+            }
+            return totalScore;
+        }
+
+        private int SummOfBallsInAFrame(int roll)
+        {
+            return rolls[roll] + rolls[roll + 1];
+        }
+
+        private int SpareBonus(int roll)
+        {
+            return rolls[roll + 2];
+        }
+
+        private int StrikeBonus(int roll)
+        {
+            return rolls[roll + 1] + rolls[roll + 2];
+        }
+        private bool IsSpare(int roll)
+        {
+            return rolls[roll] + rolls[roll + 1] == 10;
+        }
+
+        private bool IsStrike(int roll)
+        {
+            return rolls[roll] == 10;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TenPinBowling/TenPinBowlingTests/BowlingGameTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TenPinBowling;
+
+namespace TenPinBowlingTests
+{
+    [TestClass]
+    public class BowlingGameTests
+    {
+        BowlingGame game;
+        [TestInitialize]
+        public void Setup()
+        {
+            game = new BowlingGame();
+        }
+
+        public void MultipleRolls(int rolls , int pinsHit )
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                game.Roll(pinsHit);
+            }
+        }
+        [TestMethod]
+        public void NotEvenASinglePinHitShouldReturnZeroScore()
+        {
+            int rolls = 20;
+            int pinsHit = 0;
+
+            MultipleRolls(rolls, pinsHit);
+           
+            Assert.AreEqual(0, game.GetTotalScore());
+        }
+
+        [TestMethod]
+        public void AllBallsHitOnePinEachShouldReturnScoreOfTwenty()
+        {
+            int rolls = 20;
+            int pinsHit = 1;
+
+            MultipleRolls(rolls, pinsHit);
+
+            Assert.AreEqual(20, game.GetTotalScore());
+        }
+
+        [TestMethod]
+        public void BowlWithOneSpare()
+        {
+           game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+            MultipleRolls(17,0);
+            Assert.AreEqual(16,game.GetTotalScore());
+        }
+
+        [TestMethod]
+        public void FirstOneStrikeFollowedByNoSpareOrNoStrike()
+        {
+            game.Roll(10);
+            game.Roll(8);
+            game.Roll(1);
+            MultipleRolls(16, 0);
+            Assert.AreEqual(28, game.GetTotalScore());
+        }
+
+        [TestMethod]
+        public void AllStrikeWithBonusChancesBothStrike()
+        {
+            MultipleRolls(12, 10);
+            Assert.AreEqual(300, game.GetTotalScore());
+        }
+
+        [TestMethod]
+        public void Random_SpareAndStrike_WithLastStrike_AndBonusChances_NeitherStrike_NorSpare()
+        {
+            game.Roll(10);
+            game.Roll(7);
+            game.Roll(3);
+            game.Roll(9);
+            game.Roll(0);
+            game.Roll(10);
+            game.Roll(0);
+            game.Roll(8);
+            game.Roll(8);
+            game.Roll(2);
+            game.Roll(0);
+            game.Roll(6);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(8);
+            game.Roll(1);
+            Assert.AreEqual(167, game.GetTotalScore());
+        }
+    }
+}
